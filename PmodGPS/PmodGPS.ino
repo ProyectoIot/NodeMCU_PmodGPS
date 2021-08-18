5d1e5f619eb72e558c7d308ffcf326a797063a5c
Vicente RodriguezBenitez <proyecto.iot2021@gmail.com>
2021-08-18 16:40:46 -0400

    Coneccion a la red wifi

--- PmodGPS/PmodGPS.ino
+++ PmodGPS/PmodGPS.ino
@@ -1,12 +1,20 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+
+//librerias wifi
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+ 
 /*
    This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.
    It requires the use of SoftwareSerial, and assumes that you have a
    4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
 */
-#define Rx D1// TXPin
-#define Tx D2// RXPin
+#define Rx D3// TXPin
+#define Tx D4// RXPin
 
 //static const int RXPin = 4, TXPin = 3;
 static const uint32_t GPSBaud = 9600;
@@ -16,6 +24,32 @@ TinyGPSPlus gps;
 
 // The serial connection to the GPS device
 SoftwareSerial ss(Rx, Tx);
+
+//Default is AP mode.
+int wifiType = 0; // 0:Station  1:AP
+
+IPAddress staticIP(192,168,1,22);
+IPAddress gateway(192,168,1,0);
+IPAddress subnet(255,255,255,0);
+
+//Station mode you should put your ssid and password
+const char *ssid = "TP-LINK_F90A14"; // Put your SSID here
+const char *password = "11135999"; // Put your PASSWORD here
+
+  ESP8266WebServer server(80);
+
+//void handleNotFound(){
+//  String message = "Server is running!\n\n";
+//  message += "URI: ";
+//  message += server.uri();
+//  message += "\nMethod: ";
+//  message += (server.method() == HTTP_GET)?"GET":"POST";
+//  message += "\nArguments: ";
+//  message += server.args();
+//  message += "\n";
+//  server.send(200, "text/plain", message);
+//  
+//  }
 
 void setup()
 {
@@ -27,6 +61,28 @@ void setup()
   Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
   Serial.println(F("by Mikal Hart"));
   Serial.println();
+
+//  if (wifiType == 0){
+//    if(!strcmp(ssid,"TP-LINK_F90A14")){
+//       //Serial.println("Please set your SSID");
+//       while(1);
+//    }
+//    if(!strcmp(password,"11135999")){
+//       //Serial.println("Please set your PASSWORD");
+//       while(1);
+//    }
+     WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    WiFi.config(staticIP, gateway, subnet);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      //Serial.print(".");
+    }
+   Serial.println("WiFi connected");
+//    Serial.println("");
+    Serial.println(WiFi.localIP());
+    delay(5000);
+//}
 }
 
 void loop()
